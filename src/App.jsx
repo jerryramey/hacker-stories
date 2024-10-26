1d2922e0ba6731a1fa562ae647d2b0a0fbba4ebc
Jerry  Ramey <jerryramey@MacBook-Air-7.local>
2024-10-26 15:23:05 -0600

    React JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const title = 'React';
+const welcome = {
+    title: 'React',
+    greeting: 'Hey',
+};
+
+function getTitle(title) {
+  return title;
+};
 
 function App() {
 
   return (
       <div>
-          <h1>Hello React</h1>
+          <h1>{welcome.greeting} {welcome.title}</h1>
+
+          <h1>{getTitle('React')}</h1>
+
+          <label htmlFor="search">Search: </label>
+          <input id="search" type="text"/>
       </div>
   );
 
