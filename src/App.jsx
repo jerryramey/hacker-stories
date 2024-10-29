542c82f2cdf53149eaab2f6eba00c40fe5b86111
Jerry  Ramey <jerryramey@Mac-Studio.local>
2024-10-29 16:02:34 -0600

    Handler Function in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -61,10 +61,18 @@ const App = () => {
 }
 
 const Search = () => {
+    const handleChange = (event) => {
+        //synthetic event
+        console.log(event);
+
+        //value of target
+        console.log(event.target.value);
+    }
+
     return(
         <div>
             <label htmlFor="search">Search: </label>
-            <input id="search" type="text"/>
+            <input id="search" type="text" onInput={handleChange}/>
         </div>
     );
 }
