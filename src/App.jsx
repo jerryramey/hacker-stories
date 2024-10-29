99672c4b0b5f83766e6eccd9f9c20846ce723d01
Jerry  Ramey <jerryramey@Mac-Studio.local>
2024-10-29 16:23:56 -0600

    React Props

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,7 @@ const welcome = {
     greeting: 'Hey',
 };
 
-const list = [{
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-]
+
 
 function getTitle(title) {
   return title;
@@ -32,27 +16,33 @@ function getTitle(title) {
 
 const App = () => {
 
+    const stories = [{
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ]
+
   return (
       <div>
           <h1>{welcome.greeting} {welcome.title}</h1>
-
-          <h1>{getTitle('React')}</h1>
 
          <Search />
 
           <hr/>
 
-
-          <ul>
-              {list.map((item, index) => {
-                  {/* Only use array indexes as a last resort for key identifiers */
-                  }
-                  return <li key={index}>{item.title}</li>;
-              })}
-          </ul>
-
-          <h3>React Component</h3>
-          <List/>
+          <List list={stories}/>
 
 
       </div>
@@ -77,19 +67,28 @@ const Search = () => {
     );
 }
 
-const List = () => {
+const List = (props) => {
     return (
 
         <ul>
-            {list.map((item) => {
-                return <li key={item.objectID}>
-                    <span><a href={item.url} target="_blank">{item.title}</a> </span>
-                    <span> {item.author}</span>
-                    <span> {item.num_comments}</span>
-                    <span> {item.points}</span>
-                </li>;
+            {props.list.map((item) => {
+                return (
+                    <Item key={item.objectID} item={item} />
+                )
             })}
         </ul>
+    );
+}
+
+const Item = (props) => {
+
+    return (
+     <li>
+        <span><a href={props.item.url} target="_blank">{props.item.title}</a> </span>
+        <span> {props.item.author}</span>
+        <span> {props.item.num_comments}</span>
+        <span> {props.item.points}</span>
+    </li>
     );
 }
 
